b3a35c2c9463bf19b4baef939c9644d1b606f5e5
daniel <d.danielprince2003@gmail.com>
2023-02-21 22:19:19 +0530

    feat:changed views file in  eee app

--- eeeStud/views.py
+++ eeeStud/views.py
@@ -154,3 +154,27 @@ def update4(request, id):
     except eeeStud.DoesNotExist:
         # Handle the case where the student profile does not exist.
         return redirect('/eee/year4/')  # Redirect to a proper page or show an error message
+
+    if request.method == 'POST':
+        # Create a form instance with data from the POST request and the retrieved student's profile.
+        form = Year4Form(request.POST, instance=eee_stud)
+        
+        # Check if the submitted form data is valid.
+        if form.is_valid():
+            form.save()
+            return redirect('/eee/year2/')  # Redirect after successful update
+        print(form.cleaned_data)
+    else:
+        
+        # Create a form instance with the student's profile data for displaying in the template.
+        form = Year4Form(instance=eee_stud)
+        
+    return render(request, 'eeeStud/update.html', {'form': form, 'profile': eee_stud})
+
+
+
+# SEARCH
+
+
+# CREATE
+s\
