640ab8b4aa5ed26a44267ac7ecd1fd0e48631352
daniel <d.danielprince2003@gmail.com>
2023-03-04 20:35:37 +0530

    feat:changed view file

--- civilStud/views.py
+++ civilStud/views.py
@@ -13,6 +13,31 @@ def civil(request):
 
 # PROFILE
 
+def update2(request, id):
+    form=Year2Form()
+    try:
+        # Get the student's profile using the provided 'id' parameter.
+        civil_stud = civilStud2.objects.get(id=id)
+    except civilStud.DoesNotExist:
+        # Handle the case where the student profile does not exist.
+        return redirect('/civil/year2/')  # Redirect to a proper page or show an error message
+
+    if request.method == 'POST':
+        # Create a form instance with data from the POST request and the retrieved student's profile.
+        form = Year2Form(request.POST, instance=civil_stud)
+        
+        # Check if the submitted form data is valid.
+        if form.is_valid():
+            form.save()
+            return redirect('/civil/year2/')  # Redirect after successful update
+        print(form.cleaned_data)
+    else:
+        
+        # Create a form instance with the student's profile data for displaying in the template.
+        form = Year2Form(instance=civil_stud)
+        
+    return render(request, 'civilStud/update.html', {'form': form, 'profile': civil_stud})
+
 
 def update3(request, id):
     form=Year3Form()
