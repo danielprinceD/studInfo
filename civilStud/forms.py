32ae857772da7bb9dc297f6332fc9db88576e5cf
daniel <d.danielprince2003@gmail.com>
2023-03-24 20:40:06 +0530

    feat:changed form file for civil app

--- civilStud/forms.py
+++ civilStud/forms.py
@@ -3,3 +3,8 @@ from django.db import models
 from django import forms  
 from .models import civilStud,civilStud2,civilStud3,civilStud4
 
+class Year1Form(forms.ModelForm):
+    
+    class Meta:
+        model=civilStud
+        fields='__all__'
