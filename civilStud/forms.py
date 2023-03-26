51a4eb6c29e4c78857ae730bd97c58ba75499d85
daniel <d.danielprince2003@gmail.com>
2023-03-26 20:41:06 +0530

    feat:changed form file for civil app

--- civilStud/forms.py
+++ civilStud/forms.py
@@ -20,3 +20,9 @@ class Year3Form(forms.ModelForm):
     class Meta:
         model=civilStud3
         fields='__all__'
+
+class Year4Form(forms.ModelForm):
+    
+    class Meta:
+        model=civilStud4
+        fields='__all__'
