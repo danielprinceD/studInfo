9ab34faf950dbf000ef98ec3b215eb55c03eb34e
daniel <d.danielprince2003@gmail.com>
2023-03-31 20:42:40 +0530

    feat:changed admin file for civil app

--- civilStud/admin.py
+++ civilStud/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 from .models import civilStud,civilStud2,civilStud3,civilStud4
 # Register your models here.
+
+class civilAdmin(admin.ModelAdmin):
+    l='__all__'
+    
+    
