9ae9e96bcb466bdef5da88a4448f69fe42575244
daniel <d.danielprince2003@gmail.com>
2023-04-17 20:47:26 +0530

    feat:changed model file for cseStud app

--- cseStud/models.py
+++ cseStud/models.py
@@ -5,6 +5,15 @@ from django import forms
 # Create your models here.
 
     
+class cseStud3(models.Model):
+    registerNo=models.CharField(max_length=11,default=None,blank=True, null=True)
+    name=models.CharField(max_length=20,default=None,blank=True, null=True)
+    rollno=models.CharField(max_length=7,default=None,blank=True, null=True)
+    Year=models.IntegerField(default=None,blank=True, null=True)
+    Class=models.CharField(max_length=10,default=None,blank=True, null=True)
+    DOB=models.DateField(default=None,blank=True, null=True)
+    Place=models.CharField(max_length=20,default=None,blank=True, null=True)
+    
 class cseStud4(models.Model):
     registerNo=models.CharField(max_length=11,default=None,blank=True, null=True)
     name=models.CharField(max_length=20,default=None,blank=True, null=True)
