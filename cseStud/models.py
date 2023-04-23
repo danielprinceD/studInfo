2b7ba9e60a768409f6e5c7d3eb03a20de429ed33
daniel <d.danielprince2003@gmail.com>
2023-04-23 20:48:19 +0530

    feat:changed model file for cseStud app

--- cseStud/models.py
+++ cseStud/models.py
@@ -13,6 +13,14 @@ class cseStud(models.Model):
     DOB=models.DateField(default=None,blank=True, null=True)
     Place=models.CharField(max_length=20,default=None,blank=True, null=True)
     
+    YearOfJoin=models.IntegerField(default=None,blank=True, null=True)
+    Languages=models.CharField(max_length=100,default=None,blank=True, null=True)
+    FatherName=models.CharField(max_length=20,default=None,blank=True, null=True)
+    FathersOccupation=models.CharField(max_length=20,default=None,blank=True, null=True)
+    MotherName=models.CharField(max_length=20,default=None,blank=True, null=True)
+    MothersOccupation=models.CharField(max_length=20,default=None,blank=True, null=True)
+    Fees=models.IntegerField(default=None,blank=True, null=True)
+    
 class cseStud2(models.Model):
     registerNo=models.CharField(max_length=11,default=None,blank=True, null=True)
     name=models.CharField(max_length=20,default=None,blank=True, null=True)
