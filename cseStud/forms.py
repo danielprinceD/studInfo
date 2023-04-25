849883fc9e71956e124cae3579d5c6922595f462
daniel <d.danielprince2003@gmail.com>
2023-04-25 20:48:47 +0530

    feat:changed model file for cseStud app

--- cseStud/forms.py
+++ cseStud/forms.py
@@ -3,3 +3,8 @@ from django.db import models
 from django import forms  
 from .models import cseStud,cseStud2,cseStud3,cseStud4
 
+class Year1Form(forms.ModelForm):
+    
+    class Meta:
+        model=cseStud
+        fields='__all__'
