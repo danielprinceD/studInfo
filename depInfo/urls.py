743b0be8943357d51be70ab1cd6909258f0ce6e8
daniel <d.danielprince2003@gmail.com>
2023-05-04 20:51:59 +0530

    feat:changed dept app

--- depInfo/urls.py
+++ depInfo/urls.py
@@ -13,20 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.conf.urls import include
-from django.urls import path
-from depInfo import views
-
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',views.depInfo),
-    path('cse/',include('cseStud.urls')),
-    path('ece/',include('eceStud.urls')),
-    path('eee/',include('eeeStud.urls')),
-    path('civil/',include('civilStud.urls')),
-    path('mech/',include('mechStud.urls')),
-  
-
+ 
 ]
