6438fbde7ecb69e24f42374f59a5c7bf8c17a894
daniel <d.danielprince2003@gmail.com>
2023-05-11 20:52:59 +0530

    feat:changed views file in  dept app

--- depInfo/views.py
+++ depInfo/views.py
@@ -7,6 +7,10 @@ from cseStud.models import cseStud2 ,cseStud3,cseStud4
 
 from civilStud.models import civilStud
 from civilStud.models import civilStud2 ,civilStud3,civilStud4
+
+from eeeStud.models import eeeStud
+from eeeStud.models import eeeStud2 ,eeeStud3,eeeStud4
+
 # Create your views here.
 def depInfo(request):
  return render(request,'depInfo/depInfo.html')
