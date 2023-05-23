ce7b9f10d8dda9a381434e00fcbff350de8c7a39
daniel <d.danielprince2003@gmail.com>
2023-05-23 20:56:06 +0530

    feat:changed views file in  dept app

---
+++ cseStud/views.py
@@ -0,0 +1,10 @@
+from unicodedata import name
+from django.shortcuts import render,redirect
+from .models import cseStud
+from .models import cseStud2 ,cseStud3,cseStud4
+from .forms import Year1Form
+from .forms import Year2Form
+from .forms import Year3Form
+from .forms import Year4Form
+
+# Create your views here.
