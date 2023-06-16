8180a6e780bb411d1c210894f3477ab3cfb43e56
daniel <d.danielprince2003@gmail.com>
2023-06-16 21:10:54 +0530

    feat:changed views file in  ece app

--- eceStud/views.py
+++ eceStud/views.py
@@ -1,2 +1,11 @@
 from unicodedata import name
 from django.shortcuts import render,redirect
+from .models import eceStud
+from .models import eceStud2 ,eceStud3,eceStud4
+from .forms import Year1Form
+from .forms import Year2Form
+from .forms import Year3Form
+from .forms import Year4Form
+
+# Create your views here.
+
