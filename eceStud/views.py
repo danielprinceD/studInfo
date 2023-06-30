a04f2d628dbda87f4ddf34756429ae5db7ea947c
daniel <d.danielprince2003@gmail.com>
2023-06-30 21:12:30 +0530

    feat:changed views file in  ece app

--- eceStud/views.py
+++ eceStud/views.py
@@ -81,3 +81,28 @@ def delete4(request,id):
 
 # UPDATE
 
+
+def update(request, id):
+    form=Year1Form()
+    try:
+        # Get the student's profile using the provided 'id' parameter.
+        ece_stud = eceStud.objects.get(id=id)
+    except eceStud.DoesNotExist:
+        # Handle the case where the student profile does not exist.
+        return redirect('/ece/year1/')  # Redirect to a proper page or show an error message
+
+    if request.method == 'POST':
+        # Create a form instance with data from the POST request and the retrieved student's profile.
+        form = Year1Form(request.POST, instance=ece_stud)
+        
+        # Check if the submitted form data is valid.
+        if form.is_valid():
+            form.save()
+            return redirect('/ece/year1/')  # Redirect after successful update
+        print(form.cleaned_data)
+    else:
+        
+        # Create a form instance with the student's profile data for displaying in the template.
+        form = Year1Form(instance=ece_stud)
+        
+    return render(request, 'eceStud/update.html', {'form': form, 'profile': ece_stud})
