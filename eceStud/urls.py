e6d591dd3cfc102d8665ebbd53a10a86ab19c017
daniel <d.danielprince2003@gmail.com>
2023-07-16 21:14:30 +0530

    feat:changed urls file in  ece app

--- eceStud/urls.py
+++ eceStud/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 urlpatterns = [
       
 
+    path('year2/update2/<int:id>/',views.update2),
+    path('year2/delete2/<id>/',views.delete2),
     path('year2/create2/',views.create2),
 
     path('year3/',views.year3),
