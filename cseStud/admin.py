43de89ea2bf2c0efb2c021f3ad8b505b60fda7d1
DanielPrince <d.danielprince2003@gmail.com>
2023-07-23 15:48:27 +0530

    by daniel

--- cseStud/admin.py
+++ cseStud/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import cseStud
 
 # Register your models here.
+
+class cseAdmin(admin.ModelAdmin):
+    l=['registerNo','name','rollno','Year','Class','DOB','Place','FirstGraduate','YearOfJoin','Languages','FatherName','FathersOccupation','MotherName','MothersOccupation','Fees','Pending']
--- cseStud/models.py
+++ cseStud/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class cseStud(models.Model):
+    registerNo=models.CharField(max_length=11)
+    name=models.CharField(max_length=20)
+    rollno=models.CharField(max_length=6)
+    Year=models.IntegerField()
+    Class=models.CharField(max_length=10)
+    DOB=models.DateField()
+    Place=models.CharField(max_length=20)
+    FirstGraduate=models.BooleanField()
+    YearOfJoin=models.IntegerField()
+    Languages=models.CharField(max_length=100)
+    FatherName=models.CharField(max_length=20)
+    FathersOccupation=models.CharField(max_length=20)
+    MotherName=models.CharField(max_length=20)
+    MothersOccupation=models.CharField(max_length=20)
+    Fees=models.IntegerField()
+    Pending=models.BooleanField()
