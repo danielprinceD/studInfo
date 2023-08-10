c62fe444ad74a218b0d4c28af67cf59c73ef756c
daniel <d.danielprince2003@gmail.com>
2023-08-10 19:20:42 +0530

    by daniel

--- studInfo/settings.py
+++ studInfo/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import django_heroku
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -29,7 +30,7 @@ SECRET_KEY = 'django-insecure-x&-r$n5t7z4rrfzp1v!=--ekuadi7kktrf0l!0)&%3gu0v(9=&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -128,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL='static/'
 STATICFILES_DIR=[staticDir]
-
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
