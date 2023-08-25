70cff3607660c6c15a9c7ca467d815bfb665be0d
daniel <d.danielprince2003@gmail.com>
2023-08-25 21:23:20 +0530

    feat:changed views file in  eee app

--- eeeStud/views.py
+++ eeeStud/views.py
@@ -6,3 +6,8 @@ def year1(request):
     data=eeeStud.objects.all()
     sdata={'data':data}
     return render(request,'eeeStud/1year.html',context=sdata)
+
+def year2(request):
+    data=eeeStud2.objects.all()
+    sdata={'data':data}
+    return render(request,'eeeStud/2year.html',context=sdata)
