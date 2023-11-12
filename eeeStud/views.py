b69a27ed8ce21b86e4c0bf0c3292077ba866fd81
daniel <d.danielprince2003@gmail.com>
2023-11-12 21:35:23 +0530

    feat:changed views file in  eee app

--- eeeStud/views.py
+++ eeeStud/views.py
@@ -57,3 +57,16 @@ def delete2(request,id):
     delet=eeeStud2.objects.get(id=id)
     delet.delete()
     return redirect('/eee/year2/')
+
+def delete3(request,id):
+    delet=eeeStud3.objects.get(id=id)
+    delet.delete()
+    return redirect('/eee/year3/')
+
+def delete4(request,id):
+    delet=eeeStud4.objects.get(id=id)
+    delet.delete()
+    return redirect('/eee/year4/')
+    
+
+# UPDATE
